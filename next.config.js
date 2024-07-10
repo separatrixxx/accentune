45ed0808938700f846311389ddc345d783aaa67a
separatrixxx <nikitalochmatov@gmail.com>
2024-07-10 15:36:16 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,34 +1,44 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
 module.exports = {
-	i18n: {
-	  locales: ['ru'],
-	  defaultLocale: 'ru',
-	},
-	images: {
-	  domains: ['accentune.vercel.app'],
-	},
-	webpack(config) {
-	  config.module.rules.push({
-		loader: '@svgr/webpack',
-		issuer: /\.[jt]sx?$/,
-		options: {
-		  prettier: false,
-		  svgo: true,
-		  svgoConfig: {
-			plugins: [{
-			  name: 'preset-default',
-			  params: {
-				override: {
-				  removeViewBox: false
-				}
-			  }
-			}],
-		  },
-		  titleProp: true,
-		},
-		test: /\.svg$/,
-	  });
-  
-	  return config;
-	},
-  };
-  
+  i18n: {
+    locales: ['ru'],
+    defaultLocale: 'ru',
+  },
+  images: {
+    domains: ['accentune.vercel.app'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      loader: '@svgr/webpack',
+      issuer: /\.[jt]sx?$/,
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [{
+            name: 'preset-default',
+            params: {
+              override: {
+                removeViewBox: false
+              }
+            }
+          }],
+        },
+        titleProp: true,
+      },
+      test: /\.svg$/,
+    });
+
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/js/telegram-web-app.js',
+        destination: 'https://telegram.org/js/telegram-web-app.js',
+      },
+    ];
+  },
+};
