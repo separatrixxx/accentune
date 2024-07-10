0773b8b1db016ad949c153a7cb540f2c3241072e
separatrixxx <nikitalochmatov@gmail.com>
2024-07-10 15:32:08 +0300

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,14 +8,14 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700;900&display=swap" rel="stylesheet" />
+        <Script
+          src="/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
-        <Script
-          src="https://telegram.org/js/telegram-web-app.js"
-          strategy="beforeInteractive"
-        />
       </body>
     </Html>
   );
