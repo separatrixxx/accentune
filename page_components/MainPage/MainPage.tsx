e73ebedcb182de55f133b461441e2593416ef074
separatrixxx <nikitalochmatov@gmail.com>
2024-07-10 15:27:39 +0300

    update

--- page_components/MainPage/MainPage.tsx
+++ page_components/MainPage/MainPage.tsx
@@ -5,13 +5,12 @@ import { Htag } from '../../components/Common/Htag/Htag';
 import { useEffect, useState } from 'react';
 import { setLocale } from '../../helpers/locale.helper';
 
-
 interface User {
   id: number;
   first_name: string;
   last_name?: string;
   username?: string;
-};
+}
 
 export const MainPage = (): JSX.Element => {
   const router = useRouter();
@@ -20,18 +19,26 @@ export const MainPage = (): JSX.Element => {
   const [initDataUnsafe, setInitDataUnsafe] = useState<any>(null);
 
   useEffect(() => {
-    if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
-      const tg = window.Telegram.WebApp;
+    const checkTelegramWebApp = () => {
+      if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
+        const tg = window.Telegram.WebApp;
+        tg.ready();
+        console.log(tg);
 
-      tg.ready();
-      console.log(tg);
-
-      const initUser: User = tg.initDataUnsafe.user;
-      console.log(tg.initDataUnsafe);
-      setInitDataUnsafe(tg.initDataUnsafe.user);
-      setUser(initUser);
-    } else {
+        const initUser: User = tg.initDataUnsafe.user;
+        console.log(tg.initDataUnsafe);
+        setInitDataUnsafe(tg.initDataUnsafe.user);
+        setUser(initUser);
+      } else {
         setInfo('Ошибка с window.Telegram?.WebApp');
+      }
+    };
+
+    if (document.readyState === 'complete') {
+      checkTelegramWebApp();
+    } else {
+      window.addEventListener('load', checkTelegramWebApp);
+      return () => window.removeEventListener('load', checkTelegramWebApp);
     }
   }, []);
 
