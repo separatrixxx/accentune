3beb57c5798c7970fc90522390e7183d375fbe4b
separatrixxx <nikitalochmatov@gmail.com>
2024-07-10 16:31:53 +0300

    fix locale change

--- components/Common/LocaleChange/LocaleChange.tsx
+++ components/Common/LocaleChange/LocaleChange.tsx
@@ -2,7 +2,6 @@ import styles from './LocaleChange.module.css';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { useState } from 'react';
-import { en } from '../../../locales/en.locale';
 import { ru } from '../../../locales/ru.locale';
 import { setLocale } from '../../../helpers/locale.helper';
 import { Htag } from '../Htag/Htag';
@@ -14,7 +13,7 @@ export const LocaleChange = (): JSX.Element => {
 
     const [active, setActive] = useState<boolean>(false);
 
-    const languages = [en, ru];
+    const languages = [ru];
     const langIndex = languages.indexOf(setLocale(router.locale));
 
     if (langIndex !== -1) {
