46da418c7f503e75180aefee9bdd5d48cc6bc203
separatrixxx <nikitalochmatov@gmail.com>
2024-07-10 15:24:54 +0300

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
-
 export default function Document() {
   return (
     <Html>
@@ -9,14 +8,14 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700;900&display=swap" rel="stylesheet" />
-        <Script
-          src="https://telegram.org/js/telegram-web-app.js"
-          strategy="beforeInteractive"
-        />
       </Head>
       <body>
         <Main />
         <NextScript />
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
       </body>
     </Html>
   );
