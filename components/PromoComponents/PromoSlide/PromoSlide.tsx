b35df81a2e820637db66757f4f754d9540c51d60
separatrixxx <nikitalochmatov@gmail.com>
2024-08-31 01:00:58 +0300

    update promo slide

--- components/PromoComponents/PromoSlide/PromoSlide.tsx
+++ components/PromoComponents/PromoSlide/PromoSlide.tsx
@@ -7,16 +7,14 @@ import Image from 'next/image';
 export const PromoSlide = ({ photo, url }: PromoSlideProps): JSX.Element => {
 	return (
 		<Link href={url} target="_blank">
-			<div className={styles.promoSlide}>
-				<Image className={styles.promoPhoto} draggable='false'
-					loader={() => photo}
-					src={photo}
-					alt='promo image'
-					width={1}
-					height={1}
-					unoptimized={true}
-				/>
-			</div>
+			<Image className={styles.promoSlide} draggable='false'
+				loader={() => photo}
+				src={photo}
+				alt='promo image'
+				width={1}
+				height={1}
+				unoptimized={true}
+			/>
 		</Link>
 	);
 };
