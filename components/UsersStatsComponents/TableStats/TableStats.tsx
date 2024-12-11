d0967da3b0440287076d0afe92fdd9e590d1c60c
separatrixxx <nikitalochmatov@gmail.com>
2024-12-11 21:05:04 +0300

    fix table stats

--- components/UsersStatsComponents/TableStats/TableStats.tsx
+++ components/UsersStatsComponents/TableStats/TableStats.tsx
@@ -1,6 +1,7 @@
 import styles from './TableStats.module.css';
 import { useSetup } from '../../../hooks/useSetup';
 import { setLocale } from '../../../helpers/locale.helper';
+import cn from 'classnames';
 
 
 export const TableStats = (): JSX.Element => {
@@ -9,13 +10,16 @@ export const TableStats = (): JSX.Element => {
 
     const prepareUserData = () => {
         const userScores = tasks_stats.reduce((acc, task) => {
-            const taskKey = `${task.task_type} (${task.task_id})`;
+            const taskKey = `${task.task_type} (${task.task_id.slice(0, 5)})`;
+
             task.user_results.forEach(user => {
                 if (!acc[user.user_name]) {
                     acc[user.user_name] = { scores: {}, total: 0 };
                 }
-                acc[user.user_name].scores[taskKey] = user.score || 0;
-                acc[user.user_name].total += user.score || 0;
+
+                const userScore = user.score !== undefined ? user.score : 0;
+                acc[user.user_name].scores[taskKey] = userScore;
+                acc[user.user_name].total += userScore;
             });
 
             return acc;
@@ -28,8 +32,19 @@ export const TableStats = (): JSX.Element => {
         }));
     };
 
+    const sortedTasks = [...tasks_stats].sort((a, b) => {
+        const extractNumber = (str: string) => {
+            const match = str.match(/\[(\d+)]/);
+
+            return match ? parseInt(match[1], 10) : 0;
+        };
+    
+        return extractNumber(a.task_type) - extractNumber(b.task_type);
+    });
+    
+    const taskKeys = sortedTasks.map(task => `${task.task_type} (${task.task_id.slice(0, 5)})`);
     const userData = prepareUserData().sort((a, b) => a.user_name.localeCompare(b.user_name));
-    const taskKeys = tasks_stats.map(task => `${task.task_type} (${task.task_id.slice(0, 5)})`);
+
 
     return (
         <div className={styles.tableStats}>
@@ -48,7 +63,11 @@ export const TableStats = (): JSX.Element => {
                         <tr key={i}>
                             <td>{user.user_name}</td>
                             {taskKeys.map((key, j) => (
-                                <td key={j}>{user.scores[key] || 0}</td>
+                                <td key={j} className={cn({
+                                    [styles.notNullScore]: user.scores[key],
+                                })}>
+                                    {user.scores[key] !== undefined ? user.scores[key] : 0}
+                                </td>
                             ))}
                             <td>{user.total}</td>
                         </tr>
